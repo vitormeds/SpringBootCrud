c952ab10a5397d5b391d5758a0ce09a7a2fc17f0
vitormeds <vitu1221@hotmail.com>
2019-09-17 23:43:07 -0300

    deleteAndUpdate

--- src/main/java/com/vitormendes/springbootcrud/resources/ProdutoResource.java
+++ src/main/java/com/vitormendes/springbootcrud/resources/ProdutoResource.java
@@ -5,9 +5,11 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +36,16 @@ public class ProdutoResource {
 	
 	@PostMapping("/produto")
 	public Produto salvaProduto(@RequestBody @Valid Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
+	@DeleteMapping("/produto")
+	public void deletaProduto(@RequestBody @Valid Produto produto) {
+		produtoRepository.delete(produto);
+	}
+	
+	@PutMapping("/produto")
+	public Produto atualizaProduto(@RequestBody @Valid Produto produto) {
 		return produtoRepository.save(produto);
 	}
 }
